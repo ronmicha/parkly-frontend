f83d1600b0f55e91f062b92121b41cbbf42e30cf
Ron Michaeli <ron.michaeli@viz.ai>
2023-06-18 10:52:59 +0300

    Added 401 middleware

---
+++ src/hooks/useLogoutRedirect.ts
@@ -0,0 +1,16 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { addResponseMiddleware } from "../api/service";
+import { Paths } from "../navigation";
+
+export const useLogoutRedirect = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    addResponseMiddleware((response) => {
+      if (response.status === 401) {
+        navigate(Paths.LOGIN);
+      }
+    });
+  }, [navigate]);
+};
--- src/navigation/Router.tsx
+++ src/navigation/Router.tsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { routes } from "./routes";
+import { useLogoutRedirect } from "../hooks";
 
 const Router = () => {
+  useLogoutRedirect();
+
   const pageRoutes = routes.map(({ path, element }, index) => {
     return <Route key={index} path={path} element={element} />;
   });
