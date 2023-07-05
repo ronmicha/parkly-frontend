230d2f3f9a23fbedd2d5cf93aa13327d68e55415
Ron Michaeli <ron.michaeli@viz.ai>
2023-07-05 13:57:51 +0300

    UserManagement table

--- src/components/Admin/UserManagement.tsx
+++ src/components/Admin/UserManagement.tsx
@@ -1,16 +1,34 @@
+import { useEffect, useState } from "react";
+import {
+  DataGrid,
+  type GridColDef,
+  type GridEventListener,
+  GridRowEditStopReasons,
+  type GridRowModel,
+  type GridRowModesModel,
+  type GridRowsProp,
+} from "@mui/x-data-grid";
 import { useGetCustomerUsers, useGetProfile } from "../../api/domains";
-import { DataGrid, type GridColDef, type GridRowsProp } from "@mui/x-data-grid";
 
 const columns: GridColDef[] = [
-  { field: "fname", headerName: "First name", flex: 1 },
-  { field: "lname", headerName: "Last name", flex: 1 },
-  { field: "phone", headerName: "Phone number", flex: 1 },
-  { field: "email", headerName: "Email", flex: 1 },
-  { field: "admin", headerName: "Is admin?", flex: 1 },
-  { field: "vehicles", headerName: "Vehicle IDs", flex: 1 },
+  { field: "fname", headerName: "First name", editable: true, flex: 1 },
+  { field: "lname", headerName: "Last name", editable: true, flex: 1 },
+  { field: "phone", headerName: "Phone number", editable: true, flex: 1 },
+  { field: "email", headerName: "Email", editable: true, flex: 1 },
+  {
+    field: "admin",
+    headerName: "Is admin?",
+    editable: true,
+    flex: 1,
+    type: "boolean",
+  },
+  { field: "vehicles", headerName: "Vehicle IDs", editable: true, flex: 1 },
 ];
 
 export const UserManagement = () => {
+  const [rows, setRows] = useState<GridRowsProp | null>(null);
+  const [rowModesModel, setRowModesModel] = useState<GridRowModesModel>({});
+
   const { data: getProfileResponse } = useGetProfile();
 
   const { data } = useGetCustomerUsers(
@@ -18,22 +36,54 @@ export const UserManagement = () => {
     { enabled: Boolean(getProfileResponse) }
   );
 
-  const rows: GridRowsProp = (data?.users ?? []).map((user) => ({
-    id: user.id,
-    fname: user.firstName,
-    lname: user.lastName,
-    phone: user.phoneNumber,
-    email: user.email,
-    admin: user.role === "admin" ? "Yes" : "No",
-    vehicles: user.vehicleIds.join(", "),
-  }));
+  useEffect(() => {
+    if (data?.users) {
+      const rows: GridRowsProp = data.users.map((user) => ({
+        id: user.id,
+        fname: user.firstName,
+        lname: user.lastName,
+        phone: user.phoneNumber,
+        email: user.email,
+        admin: user.role === "admin",
+        vehicles: user.vehicleIds.join(", "),
+      }));
+
+      setRows(rows);
+    }
+  }, [data?.users]);
+
+  const handleRowEditStop: GridEventListener<"rowEditStop"> = (
+    params,
+    event
+  ) => {
+    if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+      event.defaultMuiPrevented = true;
+    }
+  };
+
+  const processRowUpdate = (newRow: GridRowModel) => {
+    console.count("processRowUpdate");
+    const updatedRow = { ...newRow, isNew: false };
+    setRows((prevRows) =>
+      prevRows!.map((row) => (row.id === newRow.id ? updatedRow : row))
+    );
+    return updatedRow;
+  };
 
   return (
     <>
       <h1>User Management</h1>
-      <div>
-        <DataGrid columns={columns} rows={rows} />
-      </div>
+      <DataGrid
+        rows={rows ?? []}
+        columns={columns}
+        editMode={"row"}
+        rowModesModel={rowModesModel}
+        onRowModesModelChange={setRowModesModel}
+        onRowEditStop={handleRowEditStop}
+        processRowUpdate={processRowUpdate}
+        disableRowSelectionOnClick
+        loading={rows === null}
+      />
     </>
   );
 };
